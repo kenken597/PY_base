2d95f37cdad3d7dd179ab2f71f0e02895c679283
Ivan Nesterchuk <ivan597@rambler.ru>
2021-01-22 23:33:46 +0300

    Решение второй задачи

--- Lesson_2/task_2.py
+++ Lesson_2/task_2.py
@@ -2,12 +2,15 @@
 # с индексами 0 и 1, 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input().
 
-
 user_list = []
-space = ""
+space = 0
 while space != " ":
     space = input("Введите любой элемент ")
     user_list.append(space)
 user_list.pop(-1)
-
+print(user_list)
+empt = 0
+for i in range(int(len(user_list) // 2)):
+    user_list[empt], user_list[empt + 1] = user_list[empt + 1], user_list[empt]
+    empt += 2
 print(user_list)
